2e1526240ee92c3be8fcd2ac1e85226e14ac2118
Bulat Khamitov <bulat.khamitov@gmail.com>
2019-10-10 16:17:27 +0300

    Revert "Revert "version 0.2""
    This reverts commit f0f320741ea79a5a7c63b7b37a6518e278e9cb7d.

--- carriages/passenger_carriage.rb
+++ carriages/passenger_carriage.rb
@@ -1,17 +1,9 @@
 class PassengerCarriage < Carriage
-  attr_reader :seating_capacity, :free_seats
-
   def initialize(seating_capacity)
-    super("Passenger")
-    @seating_capacity = seating_capacity
-    @free_seats = seating_capacity
+    super("Passenger", seating_capacity)
   end
 
-  def take_seat(seats = 1)
-    @free_seats -= seats unless @free_seats < seats
-  end
-
-  def occupied_seats
-    @seating_capacity - @free_seats
+  def take_space
+    super(1)
   end
 end
